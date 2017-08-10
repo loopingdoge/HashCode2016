f57f5d8499760f3ad474657223491962519e738d
Devid Farinelli <devid.farinelli@gmail.com>
2017-08-10 22:40:14 +0200

    [COMPLETED] score.py

--- src/scoring.py
+++ src/scoring.py
@@ -2,6 +2,11 @@
 #python3 src/scoring.py test googleOutputExample
 
 import sys
+import math
+from math import sqrt
+
+# CLASSES
+
 
 class Location:
     def __init__(self, r, c):
@@ -9,9 +14,11 @@ class Location:
         self.c = c                      # column
 
 class Order:
-    def __init__(self, loc, items):
+    def __init__(self, id, loc, items):
+        self.id         = id
         self.location   = loc               # Location
         self.items      = items             # {itemID:quantity}
+        self.completedTurn = None
 
     def delivered(self, itemID, quantity):
         itemQ = self.items.get(itemID, 0)
@@ -21,10 +28,17 @@ class Order:
             self.items.pop(itemID)
             if not self.items:
                 orderCompleted()
+                self.completedTurn = turn
+                print('    [COMPLETED] order{} turn{} score= {}'.format(self.id, self.completedTurn, self.getScore()))
         else:                                       # Delivered some items
             self.items[itemID] -= quantity
         # print(self.items)
 
+    def getScore(self):
+        if self.completedTurn: #if completed
+            return int(math.ceil( ( (0.0 + T - self.completedTurn) / T ) * 100) )
+            
+    
     def __repr__(self):
         return self.__str__()
 
@@ -36,8 +50,8 @@ class Order:
 
 class Warehouse:
     def __init__(self, loc, items):
-        self.location    = loc               # Location
-        self.items  = items             # {itemID:quantity}
+        self.location   = loc               # Location
+        self.items      = items             # {itemID:quantity}
 
     def getItems(self, itemID, quantity):
         itemQ = self.items.get(itemID, 0)
@@ -62,26 +76,38 @@ class Warehouse:
         return strOut
 
 class Drone:
-    items    = {}
-    commands = []                       # Command Array
-    def __init__(self, loc):
-        self.location = loc               # Location
+    def __init__(self, id, loc):
+        self.id        = id
+        self.location  = loc               # Location
+        self.items     = {}
+        self.commands  = []                # Command Array
 
     def pushCommand(self, command):
         self.commands = self.commands + [command]
         # print(self.commands)
 
+    def setLocation(self, loc):
+        self.location = loc
+        
+    def getCommand(self):
+        ret = []
+        if self.commands:
+            ret = self.commands[0]
+        return ret
+        
     def popCommand(self):
-        return self.commands.pop(0)
+        ret = []
+        if self.commands:
+            ret = self.commands.pop(0)
+        return ret
 
     def loadItems(self, itemID, quantity):
         self.items[itemID] = self.items.get(itemID, 0) + quantity
         finalW = 0  # calulate final weight
-        for ID in self.items:
-            finalW += self.items[ID] * item[ID]
-        if finalW > MAX_W:
+        for ID in self.items:    # for every item
+            finalW += self.items[ID] * weights[ID]
+        if finalW > payload:
             raise Exception('Max drone load weight passed')
-        print(self.items)
 
     def unloadItems(self, itemID, quantity):
         itemQ = self.items.get(itemID, 0)
@@ -89,14 +115,17 @@ class Drone:
             raise Exception('Not enough items carried by the drone')
         else:
             self.items[itemID] -= quantity
-            print(self.items)
             return {itemID: quantity}
 
     def __repr__(self):
         return self.__str__()
 
     def __str__(self):
-        strOut = "coords: {},{}\n".format(self.location.r, self.location.c)
+        strOut = "drone {} coords: {},{}\n".format(self.id, self.location.r, self.location.c)
+        strOut += "items: "
+        for itemID in self.items:
+            strOut += str(itemID) + ": " + str(self.items[itemID]) + ", "
+        strOut += "\n"
         for command in self.commands:
             strOut += command.getString()
         return strOut
@@ -124,47 +153,51 @@ class Command:
 def orderCompleted():
     # increments score
     global score
-    score += int( ( ( T - turn ) / T ) * 100 ) + ( T - turn ) % T > 0       # rounded up
-    print('Order completed: turn {}, score = {}'.format(turn, score))
-
+    score += int(math.ceil( ( (0.0 + T - turn) / T ) * 100) )      # rounded up
+    #print('Order completed: turn {}, score = {}'.format(turn, score))
+    
+def distance(loc1, loc2):
+    return int(math.ceil( sqrt( (loc2.r - loc1.r)**2 + (loc2.c - loc1.c)**2 ) ))
 
 # Google input parsing
-file_name	= sys.argv[1]
+file_name   = sys.argv[1]
 
 file  = open('./in/' + file_name + ".in", "r")
 
-defs = file.readline().split()
-ROWS	= int(defs[0])
-COLS	= int(defs[1])
-D 		= int(defs[2])	#DRONES
-TURNS	= int(defs[3])
-PAYLOAD = int(defs[4])
-P 		= int(file.readline())	# #PRODUCTS
-weights = [ int(x) for x in file.readline().split() ]
-W 		= int(file.readline())
-O 		= 0				# #Orders
-warehouses = []
+defs        = file.readline().split()
+ROWS        = int(defs[0])
+COLS        = int(defs[1])
+D           = int(defs[2])  #DRONES
+T           = int(defs[3])
+payload     = int(defs[4])
+P           = int(file.readline())  # #PRODUCTS
+weights     = [ int(x) for x in file.readline().split() ]
+W           = int(file.readline())
+O           = 0             # #Orders
+warehouses  = []
+orders      = []
 
+score = 0
+turn = 0
+
+warehousesList = []
 for w in range(W):
-    coords 	= [ int(x) for x in file.readline().split() ]
-    items 	= file.readline().split()
-    warehouses.append({ 'coords': coords, 'items': items})
+    coords  = [ int(x) for x in file.readline().split() ]
+    items   = file.readline().split()
+    warehousesList.append({ 'coords': coords, 'items': items})
 
 O = int(file.readline())
 
-print('ROWS:', ROWS, 'COLS:', COLS, 'DRONES:', D, 'TURNS:', TURNS, 'PAYLOAD:', PAYLOAD, 'PRODUCT TYPES', P, 'WAREHOUSES', W, 'ORDERS', O)
+print('ROWS:', ROWS, 'COLS:', COLS, 'DRONES:', D, 'T:', T, 'PAYLOAD:', payload, 'PRODUCT TYPES', P, 'WAREHOUSES', W, 'ORDERS', O)
 
-orders = []
+ordersList = []
 for o in range(O):
-    coords 	= [ int(x) for x in file.readline().split() ]
+    coords  = [ int(x) for x in file.readline().split() ]
     nitems  = int( file.readline() )
-    items 	= [ int(x) for x in file.readline().split() ] # items.length == nitems
-    orders.append({ 'coords': coords, 'items': items})
+    items   = [ int(x) for x in file.readline().split() ] # items.length == nitems
+    ordersList.append({ 'coords': coords, 'items': items})
 
 file.close()
-
-
-
 
 # Google output parsing
 file_name = sys.argv[2]
@@ -174,11 +207,11 @@ file  = open('./in/' + file_name + ".in", "r")
 defs = file.readline().split()
 
 COMMANDS_NUM = int(defs[0])
-dronesList = []      #drones array
+drones = []      #drones array
 
 for d in range(D):
-    drone = Drone(Location(0, 0))
-    dronesList = dronesList + [drone]
+    drone = Drone(d, Location(0, 0))
+    drones = drones + [drone]
 
 for c in range(COMMANDS_NUM):
     defs = file.readline().split()
@@ -193,18 +226,18 @@ for c in range(COMMANDS_NUM):
     else:
         # wait case
         command = Command(tag, data, None, None)
-    dronesList[droneID].pushCommand(command)
+    drones[droneID].pushCommand(command)
 
 file.close()
 
 #print drones
-for i,drone in enumerate(dronesList):
+for i, drone in enumerate(drones):
     print("drone {}:".format(i))
     print(drone)
 
 # initialize warehouses objects
-warehousesList = []   #warehouse array
-for i,warehouse in enumerate(warehouses):
+warehouses = []   #warehouse array
+for i, warehouse in enumerate(warehousesList):
     products = {}     #products dictionary
     r = warehouse['coords'][0]
     c = warehouse['coords'][1]
@@ -212,16 +245,16 @@ for i,warehouse in enumerate(warehouses):
         products[n] = int(warehouse['items'][n])
 
     w = Warehouse(Location(r, c), products)
-    warehousesList = warehousesList + [w]
+    warehouses = warehouses + [w]
 
 #print warehouses
-for i,warehouse in enumerate(warehousesList):
+for i,warehouse in enumerate(warehouses):
     print("warehouse {}:".format(i))
     print(warehouse)
 
 # initialize orders objects
-ordersList = []
-for i,order in enumerate(orders):
+orders = []
+for i,order in enumerate(ordersList):
     r = order['coords'][0]
     c = order['coords'][1]
     products = {}
@@ -229,10 +262,84 @@ for i,order in enumerate(orders):
     for p in range(len(order['items'])):
         products[order['items'][p]] = products.get(order['items'][p], 0) + 1
 
-    o = Order(Location(r,c), products)
-    ordersList = ordersList + [o]
+    o = Order(i, Location(r,c), products)
+    orders = orders + [o]
 
 #print orders
-for i,order in enumerate(ordersList):
+for i,order in enumerate(orders):
     print("order {}:".format(i))
     print(order)
+
+matrix = [[] for y in range(T)]    
+
+def executeCommand(drone):
+    command = drone.popCommand()
+    print('DRONE{}: {}to{} - {}#prod{} - turn={}'.format(drone.id, command.tag, command.data, command.quantity, command.product, turn))
+    if command.tag is 'L':
+        w = warehouses[command.data]
+        product = command.product
+        quantity = command.quantity
+        w.getItems(product, quantity)
+        drone.loadItems(product, quantity)
+    elif command.tag is 'D':
+        order = orders[command.data]
+        product = command.product
+        quantity = command.quantity
+        drone.unloadItems(product, quantity)
+        order.delivered(product,quantity)
+    elif command.tag is 'U':
+        w = warehouses[command.data]
+        product = command.product
+        quantity = command.quantity
+        drone.unloadItems(prodct, quantity)
+        w.putItems(product, quantity)
+    elif command.tag is 'W':
+        print("WAITING")
+        # do nothing
+
+def setDroneBusy(drone):
+    command = drone.getCommand()
+    if command:
+        if command.tag is 'L' or command.tag is 'U':
+            to_location = warehouses[command.data].location
+            delta = distance(drone.location, to_location) + 1
+            drone.setLocation(to_location)
+        elif command.tag is 'D':
+            to_location = orders[command.data].location
+            delta = distance(drone.location, to_location) + 1
+            drone.setLocation(to_location)
+        elif command.tag is 'W':
+            delta = command.data
+        matrix[turn + delta] += [drone.id]
+    
+# INIT SIMULATION
+for d in drones:
+    command = d.getCommand()
+    if command:
+        if command.tag is 'L':
+            to_location = warehouses[command.data].location
+            delta = distance(d.location, to_location)
+            d.setLocation(to_location)
+        elif command.tag is 'W':
+            delta = command.data
+        matrix[turn + delta] += [d.id]
+
+for t in range(T):
+    turn = t
+    for d in matrix[t]:
+        executeCommand(drones[d])
+        setDroneBusy(drones[d])
+    
+print('\n\nFINAL SCORE = {}'.format(score))
+    
+    
+  
+    
+    
+    
+    
+    
+    
+    
+
+
