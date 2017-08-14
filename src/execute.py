d5099d31d68a080c832d7f71452185701d186933
CecchiLinux <enrico.ceccolini@mail.com>
2017-08-14 16:41:24 +0000

    [UPDATE] execute.py, stop the execution when there is an error in the previous phase

--- src/execute.py
+++ src/execute.py
@@ -4,16 +4,37 @@ problem_name = sys.argv[1]
 
 dirname = os.path.dirname(os.path.abspath(__file__))
 
+outPlannerFilePath = "out/{}.pl".format(problem_name)
+outPlannerSolutionFilePath = "out/{}.cmds".format(problem_name)
+outParsedSolutionFilePath = "out/{}.out".format(problem_name)
+
+# remove old file if present
+if os.path.isfile(outPlannerFilePath):
+    os.remove(outPlannerFilePath)
 print("-> 1/4 PARSING INPUT FILE -> {}.in \n".format(problem_name))
 subprocess.call("python ./src/parser.py {}".format(problem_name).split())
 
-print("-> 2/4 STARTED PLANNING -> {}.pl \n".format(problem_name))
-subprocess.call("swipl -l out/{}.pl -g test -t halt -q".format(problem_name).split())
+# continue only if the previous phase is completed correctly
+if os.path.isfile(outPlannerFilePath):
+    # remove old file if present
+    if os.path.isfile(outPlannerSolutionFilePath):
+        os.remove(outPlannerSolutionFilePath)
+    print("-> 2/4 STARTED PLANNING -> {}.pl \n".format(problem_name))
+    subprocess.call("swipl -l out/{}.pl -g test -t halt -q".format(problem_name).split())
+else:
+    print("ERROR: Can't generate the planner for the input problem")
+    sys.exit(0)
 
-print("-> 3/4 PARSING OUTPUT FILE -> {}.out \n".format(problem_name))
-subprocess.call("python ./src/output.py {}".format(problem_name).split())
-
+# continue only if the previous phase is completed correctly
+if os.path.isfile(outPlannerSolutionFilePath):
+    # remove old file if present
+    if os.path.isfile(outParsedSolutionFilePath):
+        os.remove(outParsedSolutionFilePath)
+    print("-> 3/4 PARSING OUTPUT FILE -> {}.out \n".format(problem_name))
+    subprocess.call("python ./src/output.py {}".format(problem_name).split())
+else:
+    print("ERROR: Can't generate a solution for the input problem")
+    sys.exit(0)
+    
 print("-> 4/4 CALCULATING SCORE \n")
 subprocess.call("python ./src/scoring.py {}".format(problem_name).split())
-
-
